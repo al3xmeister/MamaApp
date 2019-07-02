2d13cf5a23e96c890b713ab2ac20afe77bb59eee
Alex <alex.danicel@insiso.co.uk>
2019-07-02 17:58:43 +0100

    -added appcenter nugets

--- MamaApp/MamaApp/App.xaml.cs
+++ MamaApp/MamaApp/App.xaml.cs
@@ -3,6 +3,9 @@ using MamaApp.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using MamaApp.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace MamaApp
@@ -21,6 +24,10 @@ namespace MamaApp
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("d8f823b0-9497-4973-8d4b-4c863b547c7e",
+                   typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("d8f823b0-9497-4973-8d4b-4c863b547c7e",
+                               typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
